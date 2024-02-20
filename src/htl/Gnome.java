f2ed4388bf45af0c17a26daa1278da5d3e218b47
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-02-20 16:27:18 +0100

    Added Atributes, Constructor, Getter Setters, and toString

--- src/htl/Gnome.java
+++ src/htl/Gnome.java
@@ -1,5 +1,50 @@
 package htl;
 
 public class Gnome extends CharackterTyps{
+	
+	private int attackValue;
+	private int specialAbilityProbability;
+	
+
+	public Gnome(String name, boolean specialAbilityActive) {
+		super(name, specialAbilityActive);
+		
+	}
+	
+	public int getAttackValue() {
+		return attackValue;
+	}
+
+
+	public void setAttackValue(int attackValue) {
+		this.attackValue = attackValue;
+	}
+
+
+	public int getSpecialAbilityProbability() {
+		return specialAbilityProbability;
+	}
+
+
+	public void setSpecialAbilityProbability(int specialAbilityProbability) {
+		this.specialAbilityProbability = specialAbilityProbability;
+	}
+
+
+	
+	
+	@Override
+	public String toString() {
+		return "Gnome [attackValue=" + attackValue + ", specialAbilityProbability=" + specialAbilityProbability
+				+ ", name=" + name + ", healthPoints=" + healthPoints + ", specialAbilityActive=" + specialAbilityActive
+				+ "]";
+	}
+
+
+	
+	
+	
+	
+	
 
 }
