b34eeee28b78c1f02a8931cdd30b5f9a5a80fdb4
witekTobias <tobias.witek@hak-reutte.ac.at>
2024-02-20 13:33:48 +0100

    Wir haben versucht elles Git zu repariren das ist der Neuste stand

--- src/htl/CharackterTyps.java
+++ src/htl/CharackterTyps.java
@@ -2,4 +2,39 @@ package htl;
 
 public abstract class CharackterTyps {
 
+	public String name;
+	public int healthPoints;
+	public boolean specialAbilityActive;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHealthPoints() {
+		return healthPoints;
+	}
+	public void setHealthPoints(int healthPoints) {
+		this.healthPoints = healthPoints;
+	}
+	public boolean isSpecialAbilityActive() {
+		return specialAbilityActive;
+	}
+	public void setSpecialAbilityActive(boolean specialAbilityActive) {
+		this.specialAbilityActive = specialAbilityActive;
+	}
+	
+	public CharackterTyps(String name, int healthPoints, boolean specialAbilityActive) {
+		super();
+		this.name = name;
+		this.healthPoints = healthPoints;
+		this.specialAbilityActive = specialAbilityActive;
+	}
+	
+	public void attackEnemy(Enemy) { }
+	
+	
+	
+
 }
