ed515b84ebc02bc318b9adfd538155fb29960af7
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-02-21 17:00:01 +0100

    changed the method, added sysout prompts,

--- src/htl/Gnome.java
+++ src/htl/Gnome.java
@@ -21,11 +21,6 @@ public class Gnome extends CharackterTyps{
 		this.attackValue = attackValue;
 	}
 
-
-	
-
-
-	
 	
 	@Override
 	public String toString() {
@@ -42,39 +37,36 @@ public class Gnome extends CharackterTyps{
 	public boolean dwarfHeadNut() {
 		
 		if (!this.isSpecialAbilityActive()) {
+			
 	        return false;
 	    }
 		
-		int numberOfRandomNumber = ThreadLocalRandom.current().nextInt(0, 10 + 1);
-		System.out.println(numberOfRandomNumber);
-		
-		if(this.getHealthPoints() <= 10 && numberOfRandomNumber <= 7) {
-			return true;
-		}else if(this.getHealthPoints() <= 20 && numberOfRandomNumber <= 5){
-			return true;
-		}else if(this.getHealthPoints() <= 50 && numberOfRandomNumber <= 3){
-			return true;
-		}
-		return false;
-		
-		
-	}
-	
-	public void doubleTheDamage() {
-		if(this.isSpecialAbilityActive() == true && this.dwarfHeadNut() == true) {
+		if(this.isSpecialAbilitySuccessful()) {
 			this.attackValue *= 2;
+			System.out.println("Die Zwergenkopfnuss war erfolgreich, du machst jetzt in der nächten Runde doppelt so viel schaden!");
+			return true;
+		}else {
+			this.attackValue /= 2;
+			System.out.println("Die Zwergenkopfnuss hat nicht Funktioniert du machst diese Runde nur halb soviel Schaden");
+			return false;
 		}
 	}
 	
-	public void halveTheDamage() {
-		if(this.isSpecialAbilityActive() == true && this.dwarfHeadNut() == true) {
-			this.attackValue *= 2;
-		}
-	}
 	
-	
-	
-	
+	 private boolean isSpecialAbilitySuccessful() {
+	    int numberOfRandomNumber = ThreadLocalRandom.current().nextInt(0, 10 + 1);
+	    System.out.println(numberOfRandomNumber);
+	        
+	    if (this.getHealthPoints() <= 10 && numberOfRandomNumber <= 7) {
+	       return true;
+	    } else if (this.getHealthPoints() <= 20 && numberOfRandomNumber <= 5) {
+	        return true;
+	    } else if (this.getHealthPoints() <= 50 && numberOfRandomNumber <= 3) {
+	        return true;
+	    }
+	    return false;
+	    }
+	 
 
 	
 	
