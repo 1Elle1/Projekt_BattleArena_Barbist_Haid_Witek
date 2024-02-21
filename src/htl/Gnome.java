44b5c35d633a7962bd3f93a98ad16222d36c0121
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-02-21 16:07:55 +0100

    changed the methods to one method

--- src/htl/Gnome.java
+++ src/htl/Gnome.java
@@ -5,7 +5,6 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Gnome extends CharackterTyps{
 	
 	private int attackValue;
-	private int specialAbilityProbability;
 	
 
 	public Gnome(String name, boolean specialAbilityActive) {
@@ -23,54 +22,48 @@ public class Gnome extends CharackterTyps{
 	}
 
 
-	public int getSpecialAbilityProbability() {
-		return specialAbilityProbability;
-	}
-
-
-	public void setSpecialAbilityProbability(int specialAbilityProbability) {
-		this.specialAbilityProbability = specialAbilityProbability;
-	}
+	
 
 
 	
 	
 	@Override
 	public String toString() {
-		return "Gnome [attackValue=" + attackValue + ", specialAbilityProbability=" + specialAbilityProbability
-				+ ", name=" + this.getName() + ", healthPoints=" + this.getHealthPoints() + ", specialAbilityActive=" + this.isSpecialAbilityActive()
+		return "Gnome [attackValue=" + attackValue + ", name=" + this.getName() + ", healthPoints=" 
+				+ this.getHealthPoints() + ", specialAbilityActive=" + this.isSpecialAbilityActive()
 				+ "]";
 	}
 
 
-	
-	
-	public boolean dwarfHeadNut() {
-		if(this.isSpecialAbilityActive() && this.getHealthPoints() <= 50 && this.calculateChanceOfSpecialAbility() == 3) {
-			return true;
-		}else if(this.isSpecialAbilityActive() && this.getHealthPoints() <= 20 && this.calculateChanceOfSpecialAbility() == 5) {
-			return true;
-		}else if(this.isSpecialAbilityActive() && this.getHealthPoints() <= 10 && this.calculateChanceOfSpecialAbility() == 7) {
-			return true;
-		}
-		return false;
-	}
-	
 	public void calculateAttackValue() {
 		this.attackValue = ThreadLocalRandom.current().nextInt(15, 25 + 1);
 	}
 	
-	
-	public int calculateChanceOfSpecialAbility() {
+	public boolean dwarfHeadNut() {
+		
+		if (!this.isSpecialAbilityActive()) {
+	        return false;
+	    }
+		
 		int numberOfRandomNumber = ThreadLocalRandom.current().nextInt(0, 10 + 1);
-		if(numberOfRandomNumber >= 1 && numberOfRandomNumber <=3) {
-			return 3;
-		}else if(numberOfRandomNumber >= 3 && numberOfRandomNumber <=5) {
-			return 5;
-		}else if(numberOfRandomNumber >= 5 && numberOfRandomNumber <=7){
-			return 7;
+		System.out.println(numberOfRandomNumber);
+		
+		if(this.getHealthPoints() <= 10 && numberOfRandomNumber <= 7) {
+			return true;
+		}else if(this.getHealthPoints() <= 20 && numberOfRandomNumber <= 5){
+			return true;
+		}else if(this.getHealthPoints() <= 50 && numberOfRandomNumber <= 3){
+			return true;
 		}
-		return 10;
+		return false;
+		
+		
 	}
+	
+	
+	
+	
 
+	
+	
 }
