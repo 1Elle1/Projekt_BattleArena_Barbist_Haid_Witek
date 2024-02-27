91f407f514a392c2c1e8bb495e338a58c4efda6f
HaidDerAllerEchte <jonas.haid@hak-reutte.ac.at>
2024-02-27 18:02:21 +0100

    add dragon fly

--- src/htl/Dragon.java
+++ src/htl/Dragon.java
@@ -3,7 +3,7 @@ package htl;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public  class Dragon extends CharackterTyps{
+public class Dragon extends CharackterTyps{
 	
 	private int attackValue;
 	private int specialAbilityProbability;
@@ -44,30 +44,47 @@ public  class Dragon extends CharackterTyps{
 	}
 
 	
-	//khiuhiudiuqhdiuhiuhdiu
+	
 
 	
 	
 	public void DragonFlyOn() {
 		activateSpecialSkill();
 			}
-	
 	public void DragonFlyOff() {
 		deactivateSpecialSkill();
 			}
 	
 	public void calculateAttackValue() {
 		this.attackValue = ThreadLocalRandom.current().nextInt(20, 25 + 1);
-		
-	
-	}
-	public  void attack(CharackterTyps enemy) {
+	}	
 		
 		
-	}
-	
-		public  void getDamage(int points) {
+		public boolean fly() {
 			
+			if (!this.isSpecialAbilityActive()) {
+				this.attackValue = ThreadLocalRandom.current().nextInt(5, 10 + 1);
+				
+		       
+		    }
+			 return false;
+	
+		
+		if(this.isSpecialAbilitySuccessful()) {
+			System.out.println("Die Zwergenkopfnuss war erfolgreich, du machst jetzt in dieser Runde doppelt so viel schaden!");
+			return true;
+		}else {
+			return false;
 		}
+		
+}
+		private boolean isSpecialAbilitySuccessful() {
+		    int numberOfRandomNumber = ThreadLocalRandom.current().nextInt(5, 10 + 1);
+		    System.out.println(numberOfRandomNumber);
+		}
+public  void attack(CharackterTyps enemy) {
 
 }
+	public  void getDamage(int points) {
+	}
+	}
