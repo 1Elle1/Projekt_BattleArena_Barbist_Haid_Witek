448e3d611157a89be46747a8c91d3bc6363974e1
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-02-27 12:35:18 +0100

    changed the if else

--- src/htl/Gnome.java
+++ src/htl/Gnome.java
@@ -78,11 +78,11 @@ public class Gnome extends CharackterTyps{
 	    int numberOfRandomNumber = ThreadLocalRandom.current().nextInt(0, 10 + 1);
 	    System.out.println(numberOfRandomNumber);
 	        
-	    if (this.getHealthPoints() <= 10 && numberOfRandomNumber <= 7) {
+	    if (this.getHealthPoints() <= 50 && numberOfRandomNumber <= 3) {
 	       return true;
 	    } else if (this.getHealthPoints() <= 20 && numberOfRandomNumber <= 5) {
 	        return true;
-	    } else if (this.getHealthPoints() <= 50 && numberOfRandomNumber <= 3) {
+	    } else if (this.getHealthPoints() <= 10 && numberOfRandomNumber <= 7) {
 	        return true;
 	    }
 	    return false;
