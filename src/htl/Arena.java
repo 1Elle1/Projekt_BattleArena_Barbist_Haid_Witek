341d7c0cba647e21687303de38c9dc469bd6f1ec
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-03-05 19:36:06 +0100

    Added Method pickBeginner

--- src/htl/Arena.java
+++ src/htl/Arena.java
@@ -1,12 +1,29 @@
 package htl;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Arena{
+	
+	CharackterTyps p1 = new Gnome("Gnome");
+	CharackterTyps p2 = new Dragon("Dragon");
+	
+	public static int pickBeginner() {
+		int numberOfRandomNumber = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+		if(numberOfRandomNumber == 0) {
+			return 0;
+		}else {
+			return 1;
+		}
+	}
+	
+	
 	
 	
 	
 	
 	public static void main(String[] args) {
 		
+		System.out.println(pickBeginner());
 	}
 	
 	
