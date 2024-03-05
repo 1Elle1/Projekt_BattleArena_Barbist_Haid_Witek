4a8e44d2b0962e86dffc239c957fc769bf21380e
witekTobias <tobias.witek@hak-reutte.ac.at>
2024-03-05 12:40:30 +0100

    ...

--- src/htl/Dragon.java
+++ src/htl/Dragon.java
@@ -3,18 +3,26 @@ package htl;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public  class Dragon extends CharackterTyps{
+public class Dragon extends CharackterTyps{
 	
 	private int attackValue;
 	private int specialAbilityProbability;
-	
+	public  int healthpointsDragon;
 
 	public Dragon(String name) {
 		super(name);
 		
-	}
+	} 
 	
-	public int getAttackValue() {
+	public int getHealthpointsDragon() {
+		return  healthpointsDragon;
+	}
+
+	public void setHealthpointsDragon(int healthpointsDragon) {
+		this.healthpointsDragon = healthpointsDragon;
+	}
+
+	public int getAttackValue() { 
 		return attackValue;
 	}
 
@@ -44,25 +52,41 @@ public  class Dragon extends CharackterTyps{
 	}
 
 	
-	//khiuhiudiuqhdiuhiuhdiu
+	
 
 	
 	
-	public void DragonFlyOn() {
-		activateSpecialSkill();
-			}
 	
-	public void DragonFlyOff() {
-		deactivateSpecialSkill();
-			}
 	
 	public void calculateAttackValue() {
 		this.attackValue = ThreadLocalRandom.current().nextInt(20, 25 + 1);
+	}	
 		
-	
-	
-
-	}
+		
+		public boolean fly() {
+			
+			if (!this.isSpecialAbilityActive()) {
+				this.attackValue = ThreadLocalRandom.current().nextInt(5, 10 + 1);
+				
+			 return false;
+		    }
+			 
+		if(this.isSpecialAbilitySuccessful()) {
+			this.healthpointsDragon += 10;
+			System.out.println("Der drache ist erfolgreicg abgehoben und hat 10 zusatz leben");
+			return true;
+		}else {
+			return false;
+		}
+		
+}
+		private boolean isSpecialAbilitySuccessful() {
+		    int numberOfRandomNumber = ThreadLocalRandom.current().nextInt(5, 10 + 1);
+		    System.out.println(numberOfRandomNumber);
+		    
+		    return false;
+		}
+		
 
 	@Override
 	public void attack(CharackterTyps enemy) {
