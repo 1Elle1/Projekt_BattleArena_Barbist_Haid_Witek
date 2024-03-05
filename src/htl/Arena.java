eceeba6957a41519b9bcdd34bdeb9e0f0ef5ec76
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-03-05 19:51:54 +0100

    Added method playerSelection

--- src/htl/Arena.java
+++ src/htl/Arena.java
@@ -1,8 +1,10 @@
 package htl;
 
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Arena{
+	
 	
 	CharackterTyps p1 = new Gnome("Gnome");
 	CharackterTyps p2 = new Dragon("Dragon");
@@ -16,14 +18,27 @@ public class Arena{
 		}
 	}
 	
-	
+	public static void playerSelection() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welche Charakter möchten sie spielen bitte geben sie den namen in die Komandozeile Player1:");
+		String p1Input = sc.next();
+		if(p1Input == "Gnome") {
+			System.out.println("Player1 ist der Gnome");
+		}else if(p1Input == "Dragon"){
+			System.out.println("Player1 ist der Dragon");
+		}else {
+			System.err.print("Falsche Eingabe");
+		}
+		sc.close();
+	}
 	
 	
 	
 	
 	public static void main(String[] args) {
 		
-		System.out.println(pickBeginner());
+		System.out.println();
+		
 	}
 	
 	
