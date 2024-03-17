a6bae5c578220b54a4331ece7135b5d990a36906
1Elle1 <elias.barbist@hak-reutte.ac.at>
2024-03-17 10:03:00 +0100

    changed the JavaDoc Comments and added the winnerOfTheGame

--- src/htl/CharackterTyps.java
+++ src/htl/CharackterTyps.java
@@ -1,98 +1,70 @@
 package htl;
 
-import java.util.Scanner;
 
-/**
- * This abstract class represents a character type in an arena game.
- * It defines common attributes and methods for different character types.
- */
 public abstract class CharackterTyps extends Arena {
 
-    /**
-     * The name of the character.
-     */
+
     private String name;
-
-    /**
-     * The current health points of the character.
-     */
+   
     private int healthPoints;
-
-    /**
-     * Whether the character's special ability is currently active.
-     */
+ 
     private boolean specialAbilityActive;
     
+    private boolean winnerOfTheGame;
+    
  
-    /**
-     * Creates a new character type with the given name.
-     *
-     * @param name The name of the character.
-     */
+   
     public CharackterTyps(String name) {
         super();
         this.name = name;
         this.healthPoints = 100;
         this.specialAbilityActive = false;
+        this.winnerOfTheGame = false;
     }
 
-    /**
-     * Gets the name of the character.
-     *
-     * @return The name of the character.
-     */
+   
     public String getName() {
         return name;
     }
 
-    /**
-     * Sets the name of the character.
-     *
-     * @param name The new name of the character.
-     */
+    
     public void setName(String name) {
         this.name = name;
     }
 
     public abstract int calculateAttackValue();
-    /**
-     * Gets the current health points of the character.
-     *
-     * @return The current health points of the character.
-     */
+   
     public int getHealthPoints() {
         return healthPoints;
     }
 
 
-    /**
-     * Sets the current health points of the character.
-     *
-     * @param healthPoints The new health points of the character.
-     */
     public void setHealthPoints(int healthPoints) {
         this.healthPoints = healthPoints;
     }
 
-    /**
-     * Checks if the character's special ability is currently active.
-     *
-     * @return True if the special ability is active, false otherwise.
-     */
+   
     public boolean isSpecialAbilityActive() {
         return specialAbilityActive;
     }
 
-    /**
-     * Sets the active state of the character's special ability.
-     *
-     * @param specialAbilityActive True to activate the ability, false to deactivate.
-     */
+    
     public void setSpecialAbilityActive(boolean specialAbilityActive) {
         this.specialAbilityActive = specialAbilityActive;
     }
+    
 
-    /**
+    public boolean isWinnerOfTheGame() {
+		return winnerOfTheGame;
+	}
+
+
+	public void setWinnerOfTheGame(boolean winnerOfTheGame) {
+		this.winnerOfTheGame = winnerOfTheGame;
+	}
+
+
+	/**
      * This method defines the attack behavior of the character on an enemy.
      * Specific implementation of attack functionality belongs to subclasses.
      *
