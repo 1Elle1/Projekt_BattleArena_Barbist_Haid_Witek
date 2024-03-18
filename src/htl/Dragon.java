7221a565bc66debd82c20d474306c604d021325c
HaidDerAllerEchte <jonas.haid@hak-reutte.ac.at>
2024-03-18 13:46:33 +0100

    Dragon fly

--- src/htl/Dragon.java
+++ src/htl/Dragon.java
@@ -1,86 +1,51 @@
-/**
- * The Dragon class represents a type of character in the game. 
- * It extends the CharackterTyps class and provides specific functionalities and attributes for dragons.
- */
+
 package htl;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Dragon extends CharackterTyps {
 
-    private int attackValue; // Stores the attack value of the dragon.
-    private int specialAbilityProbability; // Stores the probability of the dragon's special ability being activated.
-    private int healthpointsDragon; // Stores the health points of the dragon.
+    private int attackValue; 
+    private int specialAbilityProbability; 
+    private int healthpointsDragon; 
 
-    /**
-     * Constructor for the Dragon class.
-     * 
-     * @param name The name of the dragon.
-     */
+    
     public Dragon(String name) {
         super(name);
         this.attackValue = calculateAttackValue();
     }
 
-    /**
-     * Retrieves the health points of the dragon.
-     * 
-     * @return The health points of the dragon.
-     */
+   
     public int getHealthpointsDragon() {
         return healthpointsDragon;
     }
 
-    /**
-     * Sets the health points of the dragon.
-     * 
-     * @param healthpointsDragon The health points to set.
-     */
+   
     public void setHealthpointsDragon(int healthpointsDragon) {
         this.healthpointsDragon = healthpointsDragon;
     }
 
-    /**
-     * Retrieves the attack value of the dragon.
-     * 
-     * @return The attack value of the dragon.
-     */
+    
     public int getAttackValue() {
         return attackValue;
     }
 
-    /**
-     * Sets the attack value of the dragon.
-     * 
-     * @param attackValue The attack value to set.
-     */
+  
     public void setAttackValue(int attackValue) {
         this.attackValue = attackValue;
     }
 
-    /**
-     * Retrieves the probability of the dragon's special ability being activated.
-     * 
-     * @return The probability of the dragon's special ability being activated.
-     */
+    
     public int getSpecialAbilityProbability() {
         return specialAbilityProbability;
     }
 
-    /**
-     * Sets the probability of the dragon's special ability being activated.
-     * 
-     * @param specialAbilityProbability The probability to set.
-     */
+   
     public void setSpecialAbilityProbability(int specialAbilityProbability) {
         this.specialAbilityProbability = specialAbilityProbability;
     }
 
-    /**
-     * Overrides the toString method to provide a string representation of the Dragon object.
-     * 
-     * @return A string representation of the Dragon object.
-     */
+    
     @Override
     public String toString() {
         return "Dragon [attackValue=" + attackValue + ", specialAbilityProbability=" + specialAbilityProbability
@@ -136,12 +101,28 @@ public class Dragon extends CharackterTyps {
     @Override
     public void attack(CharackterTyps enemy) {
     	
-        if (enemy.getHealthPoints() > 0) {
-            enemy.getDamage(attackValue);
-        } else {
-            System.out.println("Spieler: " + enemy.getName() + " ist gestorben!");
-        }
-    }
+    	if (enemy.getHealthPoints() <= 0) {
+	        System.out.println(enemy.getName() + " ist bereits gestorben und kann nicht angegriffen werden.");
+	        return;
+	    }
+    	
+    	int temporaryAttackValue = this.getAttackValue();
+    	
+
+		if(this.isSpecialAbilityActive()) {
+			boolean success = fly();
+			temporaryAttackValue = success ? getHealthPoints() + 10 :0;
+			System.out.println("Du hast in dieser Runde mit der Spezialfaehigkeit " + temporaryAttackValue + " Schaden gemacht!");
+		}
+		
+		enemy.getDamage(temporaryAttackValue);
+		
+		if (enemy.getHealthPoints() <= 0) {
+	        System.out.println("Spieler: " + enemy.getName() + " ist gestorben!");
+	        
+	    }
+    	}
+    
     
     public void outputOfTheAbilities() {
 		System.out.println("1. Angreifen \n"
